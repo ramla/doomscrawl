c093bd521f1b2143282655f1a82d676d7d304109
Visa Lehtimäki <642229+ramla@users.noreply.github.com>
2026-02-12 20:35:40 +0200

    some edge cases ready to uncomment to run with

--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
-from itertools import product
+# from itertools import product
 from game import Doomcrawl
 
 default_size = (40,40)
 rooms = None
-# rooms = [((200,200), default_size),
-        #  ((300,300), default_size),
-        #  ((300,600), default_size),
-        #  ((600,600), default_size),
-        #  ((600,250), default_size),
-        #  ]
+
 # roomlocs = list(product(range(20,1000,140), range(20,660,140)))
 # roomlocs = ((580,440), (300,160), (720,160), (300,300), (720,300))
-# roomlocs = tuple(sorted(list(((559, 115), (96, 451), (358, 463), (956, 457))))) #(596, 370), (857, 291), 
+
+# I believe technically these point sets should result in concave triangulations
+# (thus 3 triangles) but I'm letting it slide since in practice it is fine
+# roomlocs = [(926, 55), (787, 55), (7, 51), (807, 59)]
+# roomlocs = [(629, 583), (880, 636), (153, 486), (856, 478)]
+# roomlocs =  [(585, 584), (1006, 215), (745, 318), (165, 533)]
+# roomlocs = [(269, 238), (585, 119), (430, 178), (182, 319)]
+# roomlocs = [(145, 247), (768, 75), (261, 256), (603, 291)]
+# roomlocs = [(594, 52), (682, 44), (410, 93), (184, 81)]
+# --
+
 # rooms = list(zip(roomlocs, [default_size]*len(roomlocs)))
+
 app = Doomcrawl(rooms)
 app.start()
