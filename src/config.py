b613b630c8fae47d67b185f68a37e7c83f802768
Visa Lehtimäki <642229+ramla@users.noreply.github.com>
2026-02-27 17:43:38 +0200

    comments, grouped interesting vars to top

--- src/config.py
+++ src/config.py
@@ -2,16 +2,27 @@ import pygame
 
 POINT_REJECTED = pygame.USEREVENT + 1
 
+viewport_x = 1200
+viewport_y = 700
+target_fps = 60
+
+                          # Heuristic is always calculated at cost of 1 per step.
+astar_step_cost = 1.4     # Larger default step cost results in more area explored and thus
+                          # (possibly) more connected corridors at the cost of more compute.
+astar_corridor_cost = 1   # Value <1 here results in an inadmissible heuristic - saves
+                          # on compute with the compromise of maybe not finding the best
+                          # path
+
 circumcircle_debug = False
-collision_debug = False
-room_debug = False  #print room list to terminal when passing room centers to 
-                    #triangulation, among other things
+collision_debug = False # show some masks related to collision, ingame shortcut 0
+room_debug = False  # print room list to terminal when passing room centers to
+                    # triangulation, among other things
 bw_debug = False
 problematic_point_debug = False
 visualizer_debug = False
 astar_debug = False # enable this to show explored area
 random_rooms = True
-draw_coords = False
+draw_coords = False # draw coordinates along with a VisualVertex
 any_debug = circumcircle_debug or \
             collision_debug or \
             bw_debug or \
@@ -19,13 +30,9 @@ any_debug = circumcircle_debug or \
             visualizer_debug or \
             astar_debug
 delay_visualisation = True
-freetype_compatibility_mode = False
+freetype_compatibility_mode = False # enable if pygame.freetype disagrees with your system
 if freetype_compatibility_mode or astar_debug:
     draw_coords = False
-
-viewport_x = 1200
-viewport_y = 700
-target_fps = 60
 
 color = {
     "light1":   pygame.Color(235,235,235),  # platinum
@@ -68,13 +75,6 @@ room_corridor_margin = corridor_width * 0.75
 vertex_radius = thickness/3
 edge_width = thickness/5
 triangle_width = thickness/5
-
-                          # Heuristic is always calculated at cost of 1 per step.
-astar_step_cost = 1.4     # Larger default step cost results in more area explored and thus
-                          # (possibly) more connected corridors at the cost of more compute.
-astar_corridor_cost = 1   # Value <1 here results in an inadmissible heuristic - saves
-                          # on compute with the compromise of maybe not finding the best
-                          # path
 
 vertex_anim_duration = 1/60
 if delay_visualisation:
